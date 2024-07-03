085aa2dd89a309e958fe5683dadaed3a6c23c38c
Daniil <samtesred@gmail.com>
2024-07-03 18:15:52 +0000

    add parser for HTML

--- browser.py
+++ browser.py
@@ -1,144 +1,210 @@
-import tkinter, tkinter.font
+"""
+This file compiles the code in Web Browser Engineering,
+up to and including Chapter 3 (Formatting Text),
+without exercises.
+"""
+
 import socket
 import ssl
+import tkinter
+import tkinter.font
+
+def request(url):
+    scheme, url = url.split("://", 1)
+    assert scheme in ["http", "https"], \
+        "Unknown scheme {}".format(scheme)
+
+    host, path = url.split("/", 1)
+    path = "/" + path
+    port = 80 if scheme == "http" else 443
+
+    if ":" in host:
+        host, port = host.split(":", 1)
+        port = int(port)
+
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    )
+    s.connect((host, port))
+
+    if scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=host)
+
+    s.send(("GET {} HTTP/1.0\r\n".format(path) +
+            "Host: {}\r\n\r\n".format(host)).encode("utf8"))
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+    statusline = response.readline()
+    version, status, explanation = statusline.split(" ", 2)
+    assert status == "200", "{}: {}".format(status, explanation)
+
+    headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n": break
+        header, value = line.split(":", 1)
+        headers[header.lower()] = value.strip()
+
+    body = response.read()
+    s.close()
+
+    return headers, body
+
+class Text:
+    def __init__(self, text, parent):
+        self.text = text
+        self.children = [] # У текстовых узлов никогда не бывает дочерних узлов, добавлено для единообразия
+        self.parent = parent
 
 
-HSTEP, VSTEP = 13, 18
-WIDTH, HEIGHT = 800, 600
-
-def layout(tokens):
-    weight = "normal"
-    style = "roman"
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for tok in tokens:
-        if isinstance(tok, Text):
-            for word in tok.text.split():
-                font = tkinter.font.Font(
-                    size=16,
-                    weight=weight,
-                    slant=style,
-                )
-                w = font.measure(word)
-                display_list.append((cursor_x, cursor_y, word, font))
-                cursor_x += w + font.measure(" ")
-                if cursor_x + w > WIDTH - HSTEP:
-                    cursor_y += font.metrics("linespace") * 1.25
-                    cursor_x = HSTEP
-        elif tok.tag == "i":
-            style = "italic"
-        elif tok.tag == "/i":
-            style ="roman"
-        elif tok.tag == "b":
-            weight = "bold"
-        elif tok.tag == "/b":
-            weight = "normal"
-    return display_list
-
-def show(body):
-    in_tag = False
-    for c in body:
-        if c == "<":
-            in_tag = True
-        elif c == ">":
-            in_tag = False
-        elif not in_tag:
-            print(c, end="")
-
-def load (url):
-    body = url.request()
-    show(body)
+class HTMLParser:
+    def __init__(self, body):
+        self.body = body
+        self.unfinished = []
     
+    def finish(self):
+        while len(self.unfifnished) > 1:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        return self.unfinished.pop()
+        
+    def parse(self):
+        text = ""
+        in_tag = False
+        for c in self.body:
+            if c == "<":
+                in_tag = True
+                if text: self.add_text(text)
+                text = ""
+            elif c == ">":
+                in_tag = False
+                self.add_tag(text)
+                text = ""
+            else:
+                text += c
+        if not in_tag and text:
+            self.add_text(text)
+        return self.finish()
+    
+    def add_text(self, text):
+        parent = self.unfinished[-1]
+        node = Text(text, parent)
+        parent.children.append(node)
+        
+    def add_tag(self, tag):
+        if tag.startswith("/"):
+            if len(self.unfinished) == 1: return
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        else:
+            parent = self.unfinished[-1] if self.unfinished else None
+            node = Element(tag, parent)
+            self.unfinished.append(node)
+
+class Element:
+    def __init__(self, tag, parent):
+        self.tag = tag
+        self.children = []
+        self.parent = parent
+
 def lex(body):
     out = []
-    buffer = ""
+    text = ""
     in_tag = False
     for c in body:
         if c == "<":
             in_tag = True
-            if buffer: out.append(Text(buffer))
-            buffer = ""
+            if text: out.append(Text(text))
+            text = ""
         elif c == ">":
             in_tag = False
-            out.append(Tag(buffer))
-            buffer = ""
+            out.append(Element(text))
+            text = ""
         else:
-            buffer += c
-    if not in_tag and buffer:
-        out.append(Text(buffer))
+            text += c
+    if not in_tag and text:
+        out.append(Text(text))
     return out
 
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
 
-class URL:
-    def __init__(self, url):
-        self.scheme, url = url.split("://", 1)
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
-        assert self.scheme in {"http", "https", "file"}
-        
-        if self.scheme == "file":
-            self.read_local_file()
-        
-        if "/" not in url:
-            url = url + "/"
+SCROLL_STEP = 100
 
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == "https":
-            self.port = 443
-        else:
-            self.port = 0
-            
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+class Layout:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.display_list = []
+
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 16
+
+        self.line = []
+        for tok in tokens:
+            self.token(tok)
+        self.flush()
+
+    def token(self, tok):
+        if isinstance(tok, Text):
+            self.text(tok.text)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
         
-    def request(self):
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP,
+    def text(self, text):
+        font = tkinter.font.Font(
+            size=self.size,
+            weight=self.weight,
+            slant=self.style,
         )
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
+        for word in text.split():
+            w = font.measure(word)
+            if self.cursor_x + w > WIDTH - HSTEP:
+                self.flush()
+            self.line.append((self.cursor_x, word, font))
+            self.cursor_x += w + font.measure(" ")
 
-        s.connect((self.host, self.port))
-        
-        request = "GET {} HTTP/1.0\r\n".format(self.path)
-        request += "Host: {}\r\n".format(self.host)
-        request += "Connection: close\r\n"
-        request += "User-Agent: Python\r\n"
-        request += "\r\n"
-        s.send(request.encode("utf8"))
-        
-        response = s.makefile("r", encoding="utf8", newline="\r\n")
-        
-        statusline = response.readline()
-        version, status, explanation = statusline.split(" ", 2)
-        
-        response_headers = {}
-        while True:
-            line = response.readline()
-            if line == "\r\n": break
-            header, value = line.split(":", 1)
-            response_headers[header.casefold()] = value.strip()
-        
-        assert "transfer-encoding" not in response_headers
-        assert "content-encoding" not in response_headers
-        
-        content = response.read()
-        s.close()
-        
-        return content
-    
-    def read_local_file(self):
-        with open(self.path, "r") as f:
-            return f.read()
-    
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.2 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        self.cursor_x = HSTEP
+        self.line = []
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.2 * max_descent
+
 class Browser:
     def __init__(self):
-        self.SCROLL_STEP = 50
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(
             self.window,
@@ -146,60 +212,35 @@ class Browser:
             height=HEIGHT
         )
         self.canvas.pack()
+
         self.scroll = 0
         self.window.bind("<Down>", self.scrolldown)
-        self.window.bind("<Up>", self.scrollup)
-        self.window.bind("<Button-4>", self.scrollup)
         self.window.bind("<Button-5>", self.scrolldown)
-        
+        self.window.bind("<Button-4>", self.scrollup)
+        self.display_list = []
+
     def load(self, url):
-        body = url.request()
-        text = lex(body)
-        self.display_list = layout(text)
-        self.draw()
+        headers, body = request(url)
+        tokens = lex(body)
+        self.display_list = Layout(tokens).display_list
+        self.render()
 
-    def scrollup(self, e):
-        self.scroll -= self.SCROLL_STEP
-        self.draw()
-    
-    def scrolldown(self, e):
-        self.scroll += self.SCROLL_STEP
-        self.draw()
-
-        # self.canvas.create_oval(100, 100, 150, 150)
-#                                x1,  y1,  x2,  y2 
-    
-    def draw(self):
+    def render(self):
         self.canvas.delete("all")
-        for x, y, c, f in self.display_list:
+        for x, y, word, font in self.display_list:
             if y > self.scroll + HEIGHT: continue
-            if y + VSTEP < self.scroll: continue
-            self.canvas.create_text(x, y - self.scroll, text=c, font=f)
+            if y + font.metrics("linespace") < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=word, font=font, anchor="nw")
 
-
-class Text:
-    def __init__(self, text):
-        self.text = text
-
-
-class Tag:
-    def __init__(self, tag):
-        self.tag = tag
-
-
-class Layout:
-    pass
-
-
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.render()
+    
+    def scrollup(self, e):
+        self.scroll -= SCROLL_STEP
+        self.render()
+        
 if __name__ == "__main__":
     import sys
-    Browser().load(URL(sys.argv[1]))
+    Browser().load(sys.argv[1])
     tkinter.mainloop()
-
-# if __name__ == "__main__":
-#     import sys
-#     try:
-#         load(URL(sys.argv[1]))
-#     except IndexError:
-#         load(URL("file:///home/bolshoy/Desktop/test.txt"))
-    
